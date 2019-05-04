86e2370a2965f8a91e50c1fcddfcbe25b5a99159
jonathan canaveral <neumerance@live.com>
2019-05-04 16:01:08 +0800

    authentication routine

--- __tests__/screens/LoginScreenSpec.js
+++ __tests__/screens/LoginScreenSpec.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import LoginScreen from '../../screens/LoginScreen';
 
 describe('LoginScreen', () => {
   it('renders correctly', () => {
-    const LoginScreenComponent = renderer.create(<LoginScreen />).toJSON();
-    expect(LoginScreenComponent).toMatchSnapshot();
+    const component = shallow(<LoginScreen />);
+    expect(component).toMatchSnapshot();
   });
 });
